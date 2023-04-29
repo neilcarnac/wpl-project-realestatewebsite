e8d5990ae04077630b49db7bb9eafc981f553872
Neil <neil.carnac@somaiya.edu>
2023-04-29 17:22:27 +0530

    :made changes to the contact page added really cool button

---
+++ .history/project/components/footer_20230429162413.php
@@ -0,0 +1,37 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>1112223333</span></a>
+         <a href="mailto:neil.carnac@somaiya.edu"><i class="fas fa-envelope"></i><span>neil.carnac@somaiya.edu</span></a>
+         <a href="mailto:aditya.patil7@somaiya.edu"><i class="fas fa-envelope"></i><span>aditya.patil7@somaiya.edu</span></a>
+         <a href="mailto:sakshi.borade@somaiya.edu"><i class="fas fa-envelope"></i><span>sakshi.borade@somaiya.edu</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>mumbai, india - 400104</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
