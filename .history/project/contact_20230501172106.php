965464988fcbb1e3aace07054cc09c442fab1bbc
Neil <neil.carnac@somaiya.edu>
2023-05-02 11:11:08 +0530

    :almost final

---
+++ .history/project/contact_20230501172106.php
@@ -0,0 +1,208 @@
+<?php  
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+if(isset($_POST['send'])){
+
+   $msg_id = create_unique_id();
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $number = $_POST['number'];
+   $number = filter_var($number, FILTER_SANITIZE_STRING);
+   $message = $_POST['message'];
+   $message = filter_var($message, FILTER_SANITIZE_STRING);
+
+   $verify_contact = $conn->prepare("SELECT * FROM `messages` WHERE name = ? AND email = ? AND number = ? AND message = ?");
+   $verify_contact->execute([$name, $email, $number, $message]);
+
+   if($verify_contact->rowCount() > 0){
+      $warning_msg[] = 'message sent already!';
+   }else{
+      $send_message = $conn->prepare("INSERT INTO `messages`(id, name, email, number, message) VALUES(?,?,?,?,?)");
+      $send_message->execute([$msg_id, $name, $email, $number, $message]);
+      $success_msg[] = 'message send successfully!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Contact Us</title>
+   <link type="image/png" sizes="16x16" rel="icon" href="images/hoe.png">
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<div>
+<!-- contact section starts  -->
+<style>
+		button {
+			background-color: #da3332;
+			color: black;
+			padding: 10px 20px;
+			border: none;
+			border-radius: 5px;
+			font-size: 1.3rem;
+         /* margin-top: 500px; */
+         margin-left: 660px;
+         font-weight: bold;
+         border: 1.5px solid black;
+         position: absolute;
+         margin-top: 260px;
+         /* font-family: Montserrat; Change the font */
+         transition: background-color 0.3s, color 0.3s;
+         transform: translateX(-50%); /* Adjust for half of the button's width */
+
+         
+		}
+      button:hover{
+         cursor: pointer;
+         background-color: #EEEEEE;
+			color: black;
+      }
+      .selele{
+         
+         position: absolute; /* Add relative positioning to the container */
+			height: 1000px;
+      }
+      .finger{
+         transition: 600ms;
+         position: absolute; /* Add relative positioning to the container */
+         height:100px ;
+         width:45px ;
+         margin-top: 250px;
+         margin-left: 660px;
+         z-index: 2;
+      }
+      .finger:hover{
+         cursor: pointer;
+         transform: scale(0.9); /* Add a scale transform on hover */
+      }
+      .linke{
+         text-decoration: none;
+         color: black;
+      }
+      .touch{
+         margin-left: 40px ;
+         margin-top: -40px;
+         transition: 1000ms;
+      }
+      .getin{
+         margin-right: 130px;
+      }
+      .touch:hover{
+         transform: scale(0.9); /* Add a scale transform on hover */
+         cursor: pointer;
+      }
+      
+	</style>
+
+   <div class="selele">
+      <img src="images/fingerwhite.png" class="finger" alt="">
+      <button><a class="linke" href="home.php"><b>SELECT</b></a></button>
+   </div>
+<section class="contact">
+   <div class="row">
+      <div class="image">
+         <img src="images/contact-img.svg" alt="">
+      </div>
+      <form action="" method="post">
+         <h3 class="getin">get in <h3  class="touch">touch</h3></h3>
+         <input type="text" name="name" required maxlength="50" placeholder="enter your name" class="box">
+         <input type="email" name="email" required maxlength="50" placeholder="enter your email" class="box">
+         <input type="number" name="number" required maxlength="10" max="9999999999" min="0" placeholder="enter your number" class="box">
+         <textarea name="message" placeholder="enter your message" required maxlength="1000" cols="30" rows="10" class="box"></textarea>
+         <input type="submit" value="send message" name="send" class="btn">
+      </form>
+   </div>
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- faq section starts  -->
+
+<section class="faq" id="faq">
+
+   <h1 class="heading">FAQ</h1>
+
+   <div class="box-container">
+
+      <div class="box active">
+         <h3><span>how to cancel booking?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+      </div>
+
+      <div class="box active">
+         <h3><span>when will I get the possession?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+      </div>
+
+      <div class="box">
+         <h3><span>where can I pay the rent?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+      </div>
+
+      <div class="box">
+         <h3><span>how to contact with the buyers?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+      </div>
+
+      <div class="box">
+         <h3><span>why my listing not showing up?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+      </div>
+
+      <div class="box">
+         <h3><span>how to promote my listing?</span><i class="fas fa-angle-down"></i></h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis ducimus aut accusantium sunt error esse laborum cumque ipsum ab.</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- faq section ends -->
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<?php include 'components/message.php'; ?>
+</div>
+</body>
+</html>
